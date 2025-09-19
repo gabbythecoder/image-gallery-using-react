9c97ae22f3de561e29a047b9db6dae9d7059a6b7
gabby <hannahfrench1709@hotmail.com>
2025-09-19 10:37:47 +0100

    feat: add largeimage folder in components folder to tidy up

--- src/components/largeimage/LargeImage.jsx
+++ src/components/largeimage/LargeImage.jsx
@@ -1,9 +1,8 @@
 import "./LargeImage.css";
 
-export default function LargeImage({ src, alt, title }) {
+export default function LargeImage({ src, alt }) {
     return (
         <div className="large-image-container">
-            <p>{title}</p>
             <img src={src} alt={alt} className="large-image"/>
         </div>
     );
