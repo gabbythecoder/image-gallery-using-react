1ef0d52cb49d05945d9521dd8797d7dbef874e4b
gabby <hannahfrench1709@hotmail.com>
2025-09-19 17:03:20 +0100

    feat: add header and footer component

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <p className="footer-text">&copy; Gabby French | Made with ❤️ in the UK</p>
+        </footer>
+    )
+}
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,9 @@
+import "./Header.css";
+
+export default function Header() {
+    return (
+        <header className="header">
+            <h1 className="main-title">Image Gallery</h1>
+        </header>
+    )
+}
