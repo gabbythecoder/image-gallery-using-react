62612df294e8dbe6cc3064fedeb2442b30cd5f8f
gabby <hannahfrench1709@hotmail.com>
2025-09-19 10:34:39 +0100

    feat: update Thumbnail component with unsplash API

---
+++ src/components/thumbnailimages/ThumbnailImages.jsx
@@ -0,0 +1,22 @@
+import Thumbnail from "./Thumbnail.jsx";
+import "./ThumbnailImages.css";
+
+export default function ThumbnailImages({ images, onSelectImage }) {
+  function handleClick(index) {
+    onSelectImage(index);
+  }
+
+  return (
+    <div className="thumbnail-container">
+      {images.map((image, index) => (
+        <Thumbnail
+          key={image.id}
+          src={image.urls.thumb}
+          alt={image.alt_description}
+          onClick={() => handleClick(index)}
+          className="thumbnail-item"
+        />
+      ))}
+    </div>
+  );
+}
